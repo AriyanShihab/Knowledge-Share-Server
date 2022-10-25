7d1ce72e59fe646ecb0e39944e32ea815e00a7c4
AriyanShihab <devshihab111@gmail.com>
2022-10-25 12:14:43 +0600

    get course by id, and featured course api done

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 const port = process.env.PORT || 5000;
-const data = require("./data/data.json");
+const courses = require("./data/data.json");
 
-app.get("/", (req, res) => {
-  res.send(data);
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const exactCourse = courses.find((course) => course.id === id);
+  res.send(exactCourse);
+});
+app.get("/featuredCourses", (req, res) => {
+  const onlyFeatured = courses.filter((course) => course.isFeatured === true);
+
+  if (!onlyFeatured) {
+    res.send("no data found");
+  }
+  res.send(onlyFeatured);
 });
 
 app.listen(port, () => {
